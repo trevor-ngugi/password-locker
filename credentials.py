49e8cef89e8743d30f0f45ba85063c55e6f7ceae
trevor ngugi <ngugitrevor@gmail.com>
2019-12-14 16:09:56 +0300

    add credential propeties

---
+++ credentials.py
@@ -0,0 +1,11 @@
+class Credentials:
+    """
+    class that generates  social app name,username,and password properties
+    """
+    credentials_list=[]
+
+    def __init__(self,social_app_name,app_username,app_password):
+        self.social_app_name=social_app_name
+        self.app_username=app_username
+        self.app_password=app_password
+        
